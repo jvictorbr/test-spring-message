bd76c696e33b117b5e8bd013525cfa18245bd94c
jr23508 <jr23508@LACBRA900L0007.lac.nsroot.net>
2014-10-10 16:04:55 -0300

    commited.

---
+++ src/main/java/com/citi/test/controller/HelloMessage.java
@@ -0,0 +1,11 @@
+package com.citi.test.controller;
+
+public class HelloMessage {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+}
---
+++ src/main/java/com/citi/test/startup/AppConfig.java
@@ -0,0 +1,10 @@
+package com.citi.test.startup;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.citi.test")
+public class AppConfig {
+
+}
